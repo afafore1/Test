7bde59e99ba05920d6bccd7096afab95247f4eb8
Ayomitunde Fafore <afafore1@student.gsu.edu>
2015-08-29 16:48:43 -0700

    Updated..
    3 solutions, different run time..

--- facebook1.java
+++ facebook1.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+
 import org.apache.commons.lang.ArrayUtils;
 
 
@@ -8,14 +9,49 @@ public class facebook1 {
 		for(int l = 0; l < r; l++){
 			if(nums[l] == 0){
 				nums = ArrayUtils.remove(nums, l);
+			}else if(nums[r] == 0){
+				nums = ArrayUtils.remove(nums, r);
+			}else{
+				r--;
 			}
 		}
 		System.out.println(Arrays.toString(nums));
 	}
+	
+	public static void delNums(int [] nums, int thenum){
+		int r = nums.length -1;
+		int count = 1;
+		for(int l = 0; l < r; l++){
+			if(nums[l] == 0){
+				for(int x = l; x < r; x++){
+					nums[x] = nums[x] ^ nums[x+1];
+					nums[x+1] = nums[x] ^ nums[x+1];
+					nums[x] = nums[x] ^ nums[x+1];
+				}
+				r--;
+				l--;
+			}else{
+				count++;
+			}
+		}
+		//nums = Arrays.copyOfRange(nums, 0, count);  this could be used too
+		nums = Arrays.copyOf(nums, count);
+		System.out.println(Arrays.toString(nums));
+	}
 
 	public static void main(String[] args) {
-		long start = System.nanoTime();
+		
 		int [] givenArray = new int[] {3,4,45,5,0,3,0,32,4,5,40,23,0,43,0,0,43,2,4};
+		long start = System.nanoTime();
+		Deletenum(givenArray,0);
+		long end = System.nanoTime();
+		System.out.println("Time taken for Deletenum "+(end-start)+" ns\n");
+		
+		start = System.nanoTime();
+		delNums(givenArray,0);
+		end = System.nanoTime();
+		System.out.println("Time taken for delNums is "+(end-start)+" ns\n");
+		start = System.nanoTime();
 		int rightPointer = givenArray.length -1;
 		for(int leftPointer = 0; leftPointer < rightPointer;leftPointer++){
 			if(givenArray[leftPointer] == 0){
@@ -35,13 +71,10 @@ public class facebook1 {
 				givenArray = tempArray;
 			}
 		}
-		long end = System.nanoTime();
-		System.out.println(Arrays.toString(givenArray));
-		System.out.println("Time taken "+(end-start)+" ns");
-		start = System.nanoTime();
-		Deletenum(givenArray,0);
 		end = System.nanoTime();
-		System.out.println("Time taken for Deletenum "+(end-start)+" ns");
+		System.out.println(Arrays.toString(givenArray));
+		System.out.println("Time taken for shifting and copying array back is "+(end-start)+" ns\n");
+	
 
 
 	}
